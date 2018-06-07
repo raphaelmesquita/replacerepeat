fa1443b42920c5b27976eef6cd0e43954abbe388
Raphael Lima <rlima@gaveainvest.com.br>
2018-06-07 18:01:27 -0300

    Some corrections

--- Program.fs
+++ Program.fs
@@ -6,17 +6,17 @@ open Result.Operators
 type Arguments =
     | [<ExactlyOnce; AltCommandLine("-t")>]
       Template of path:string
-    | [<ExactlyOnce; AltCommandLine("-o")>]
-      Output of path:string
     | [<ExactlyOnce; AltCommandLine("-r")>]
       Replacements of path:string
+    | [<ExactlyOnce; AltCommandLine("-o")>]
+      Output of path:string
 with
     interface IArgParserTemplate with
         member s.Usage =
             match s with
             | Template _ -> "Specify a template file."
-            | Output _ -> "Specify an output file."
             | Replacements _ -> "Specify a replacements file. This file must have the search strings on the first line and the replacements on the following lines. If more than one term is to be replaced, separate both search string and replacements by double pipe (||). \nExample content:\nOld-A||Old-B||Old-C\nNew-A1||New-B1||New-C1\nNew-A2||New-B2||New-C2"
+            | Output _ -> "Specify an output file."
 
 type Replacement = 
     { Old: string
@@ -49,7 +49,7 @@ let splitLine (line: string) =
 let parseReplacements (textLines: string list) =
     match textLines with
     | [] -> 
-        Error "The replacements file is empty"
+        Error "The replacements file is empty."
     | firstLine :: replacementLines ->
         let oldStrings = splitLine firstLine
         try
@@ -61,7 +61,11 @@ let parseReplacements (textLines: string list) =
             replacementLines
             |> List.map getReplacements
             |> Ok
-        with e -> e.Message |> Error
+        with 
+        | :? ArgumentException ->
+            "Invalid replacements file: searchString and replacements don't match in length." |> Error
+        | e -> 
+            e.Message |> Error
 
 let replaceAndRepeat templatePath outputPath replacementsPath =
     result {
@@ -84,7 +88,7 @@ let main argv =
 
     match replaceAndRepeat templatePath outputPath replacementsPath with
     | Ok _ -> 
-        printf "Arquivo %s gravado com sucesso." outputPath
+        printf "Output file %s successfully created." outputPath
         0
     | Error msg ->
         printf "%s" msg
